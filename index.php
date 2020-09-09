e28e118db0e0226e5e6140b1e03def7381a02265
Navika-M <47578845+Navika-M@users.noreply.github.com>
2020-09-09 13:08:40 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php 
+
+$products = [
+    ['name' => 'shiny star', 'price' => 20],
+    ['name' => 'green shell', 'price' => 10],
+    ['name' => 'red shell', 'price' => 15],
+    ['name' => 'gold coin', 'price' => 5],
+    ['name' => 'lightning bolt', 'price' => 40],
+    ['name' => 'banana skin', 'price' => 2],
+];
+
+$i = 0;
+
+while($i < count($products)){
+    echo $products[$i]['name'];
+    echo '<br />';
+    $i++;
+}
+
+// comparisons booleans (true or false)
+
+
+// loose vs strict equal comparison 
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP Tutorials</title>
+</head>
+
+<body>
+
+
+</body>
+</html>
