2fa802f0b915b57723e619ffde0006cf829640bd
Navika-M <47578845+Navika-M@users.noreply.github.com>
2020-09-09 13:26:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,50 @@
 <?php 
 
-$products = [
-    ['name' => 'shiny star', 'price' => 20],
-    ['name' => 'green shell', 'price' => 10],
-    ['name' => 'red shell', 'price' => 15],
-    ['name' => 'gold coin', 'price' => 5],
-    ['name' => 'lightning bolt', 'price' => 40],
-    ['name' => 'banana skin', 'price' => 2],
-];
+// $products = [
+//     ['name' => 'shiny star', 'price' => 20],
+//     ['name' => 'green shell', 'price' => 10],
+//     ['name' => 'red shell', 'price' => 15],
+//     ['name' => 'gold coin', 'price' => 5],
+//     ['name' => 'lightning bolt', 'price' => 40],
+//     ['name' => 'banana skin', 'price' => 2],
+// ];
 
-$i = 0;
-
-while($i < count($products)){
-    echo $products[$i]['name'];
-    echo '<br />';
-    $i++;
-}
 
 // comparisons booleans (true or false)
+// echo false;
+
+// numbers
+
+// echo 5 < 10;
+// echo 5 > 10;
+// echo 5 == 10;
+// echo 10 == 10;
+// echo 5 != 10;
+// echo 5 <= 5;
+// echo 5 >= 5;
+
+
+
+// strings
+// echo 'shaun' < 'yoshi';
+// echo 'shaun' > 'yoshi';
+// echo 'shaun' > 'Shaun';
+// echo 'mario'  == 'mario';
+// echo 'mario' == 'Mario'
+
 
 
 // loose vs strict equal comparison 
+// echo 5 == '5';
+// echo 5 === '5';
+// echo 5 === 5;
+
+// echo true == '1';
+echo false == '';
+
+
+
+
 
 
 ?>
