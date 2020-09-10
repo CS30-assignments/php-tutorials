70d3640c6a37926da669dffa89364de149d0ffa3
Navika-M <47578845+Navika-M@users.noreply.github.com>
2020-09-10 14:39:03 -0600

    created header and footer

--- index.php
+++ index.php
@@ -1,32 +1,14 @@
 <?php 
 
-// include
-// include('ninjas.php');
-// require('ninjas.php');
-
-require 'ninjas.php';
-include 'ninjas.php';
-
-echo 'end of php';
-
-
 ?>
 
 
 <!DOCTYPE html>
 <html>
 
-<head>
-    <title>PHP Tutorials</title>
-</head>
-
-<body>
-
-    <?php include('content.php'); ?>
-    <?php include('content.php'); ?>
-    <?php include('content.php'); ?>
+    <?php include('header.php'); ?>
+    <?php include('footer.php'); ?>
 
 
-</body>
 
 </html>
