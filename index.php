abefdf17d0482b721d7fd1748c176e7f950ea97e
Navika-M <47578845+Navika-M@users.noreply.github.com>
2020-09-10 13:06:38 -0600

    include and require

--- index.php
+++ index.php
@@ -1,43 +1,6 @@
 <?php 
 
-// variable scope
-
-// local variables
-
-function myFunc(){
-    $price = 10;
-    echo $price;
-}
-
-// myFunc();
-// echo $price;
-
-function myFuncTwo($age){
-    echo $age;
-}
-
-// myFuncTwo(25);
-
-// gloabl variables
-$name = 'mario';
-
-// function sayHello(){
-//     global $name;
-//     $name = 'yoshi';
-//     echo "hello $name";
-// }
-
-// sayHello();
-// echo $name;
-
-
-function sayBye(&$name){
-    $name = 'wario';
-    echo "bye $name";
-}
-
-sayBye($name);
-echo $name;
+// include
 
 ?>
 
